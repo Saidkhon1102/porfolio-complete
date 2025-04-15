39f66bec9d5e092967cef5f561129ddc87d1e0f8
Saidxon <saidxon@gmail.com>
2025-04-15 13:07:35 +0500

    portfolio-complete

---
+++ js/data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: "Louvre Museum Website(background video & position lesson)",
+    github: "https://github.com/Saidkhon1102/Louvre-lesson-website",
+    vercel: "https://louvre-lesson-website.vercel.app",
+  },
+  {
+    title: "Example",
+    github: "example.uz",
+    vercel: "example.uz",
+  },
+];

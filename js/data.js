0639f9b04c1c58c46f8d8fb56f6e98a6ca2a0442
Saidxon <saidxon@gmail.com>
2025-04-18 16:08:16 +0500

    portfolio

--- js/data.js
+++ js/data.js
@@ -1,5 +1,20 @@
 export default [
   {
+    title: "Form-action-website",
+    github: "https://github.com/Saidkhon1102/form-action-website.git",
+    vercel: "https://form-action-website.vercel.app",
+  },
+  {
+    title: "Intro main section(hover)",
+    github: "https://github.com/Saidkhon1102/intro-main-section-hover-.git",
+    vercel: "https://intro-main-section-hover.vercel.app",
+  },
+  {
+    title: "Simple receipt website (position lesson)",
+    github: "https://github.com/Saidkhon1102/simple-omlet-recept.git",
+    vercel: "https://simple-omlet-recept.vercel.app",
+  },
+  {
     title: "Louvre Museum Website(background video & position lesson)",
     github: "https://github.com/Saidkhon1102/Louvre-lesson-website",
     vercel: "https://louvre-lesson-website.vercel.app",
